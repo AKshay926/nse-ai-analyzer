b3b3eb3d35cb3fc2981a99921c63ad7192c8dd35
Akshay Surange <akshanku@gmail.com>
2026-05-06 17:24:44 +0530

    Updated PulseIQ login branding

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ if not st.session_state["logged_in"]:
     
    # ***⚡ PulseIQ***
 
-   ### Real-Time Option Chain Intelligence for Smarter Trading
+   ### ***Real-Time Option Chain Intelligence for Smarter Trading***
 
    🔐 Secure Zerodha Login *(Once Daily)*
     """)
