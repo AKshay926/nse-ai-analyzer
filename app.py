596065e5a401c0c511fa5c23d308b6d036f02225
Akshay Surange <akshanku@gmail.com>
2026-05-06 17:49:16 +0530

    Updated PulseIQ login branding

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.markdown("""
 .stApp {
     background-image:
     linear-gradient(
-        rgba(0, 0, 0, 0.65),
+        rgba(0, 0, 0, 0.58),
         rgba(0, 0, 0, 0.90)
     ),
     url("https://images.unsplash.com/photo-1642790106117-e829e14a795f");
